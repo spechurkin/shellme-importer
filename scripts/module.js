0ce9c7c4e3d40058f1cf7531e4cdabc77acfbc0f
Svyatoslav Pechurkin <pechurkins@gmail.com>
2026-08-02 12:37:50 +0300

    1.0.2

--- scripts/module.js
+++ scripts/module.js
@@ -4,7 +4,12 @@ import {logAction, logError} from "./logger.js";
 Hooks.on("getActorSheetHeaderButtons", getActorSheetHeaderButtons);
 
 function getActorSheetHeaderButtons(sheet, buttons) {
-    if (!game.user.can("FILES_UPLOAD")) return;
+    const {CPRCharacterActorSheet, CPRMookActorSheet} = game.cpr?.apps ?? {};
+    const isImportSheet =
+        (CPRCharacterActorSheet && sheet instanceof CPRCharacterActorSheet)
+        || (CPRMookActorSheet && sheet instanceof CPRMookActorSheet);
+
+    if (!game.user.can("FILES_UPLOAD") || !isImportSheet) return;
 
     buttons.unshift({
         label: game.i18n.localize("ShellMeImporter.Button"),
